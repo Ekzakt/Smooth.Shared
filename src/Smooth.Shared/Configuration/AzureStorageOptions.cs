a03f310cae5b7c08c3d878b000fc56b13c140650
EricJansen <EricJansen@LAPTOP-EJ>
2025-01-27 15:56:45 +0100

    Added properties to AzureStorageOptions.

--- src/Smooth.Shared/Configuration/AzureStorageOptions.cs
+++ src/Smooth.Shared/Configuration/AzureStorageOptions.cs
@@ -7,4 +7,8 @@ public class AzureStorageOptions
     public const string SectionName = "Azure:Storage";
 
     public string ServiceUri { get; set; }
+
+    public string AccountName { get; set; }
+
+    public string AccountKey { get; set; }
 }
